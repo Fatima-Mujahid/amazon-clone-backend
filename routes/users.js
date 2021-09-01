774b55946609cd5c21f21a268526b26db7a8e644
Fatima-Mujahid <65939673+Fatima-Mujahid@users.noreply.github.com>
2021-09-01 10:16:36 +0500

    feat(users): create users routes

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,22 @@
+import express from 'express';
+import { registerUser, loginUser } from '../controllers/users.js';
 
+const router = express.Router({ mergeParams: true });
+
+//REGISTER ROUTE
+/**
+ * @route   POST /user/register
+ * @desc    Register user
+ * @body    {email, password}
+ */
+router.post('./register', registerUser);
+
+//LOGIN ROUTE
+/**
+ * @route   POST /user/login
+ * @desc    Login user
+ * @body    {email, password}
+ */
+router.post('./login', loginUser);
+
+export default router;
