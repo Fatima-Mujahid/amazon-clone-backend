633b0761365aae6f38c4dff59be02e1cd1b933b8
Fatima-Mujahid <65939673+Fatima-Mujahid@users.noreply.github.com>
2021-09-10 23:28:08 +0500

    feat(orders): create orders controllers

---
+++ controllers/orders.js
@@ -0,0 +1,33 @@
+import Order from '../models/order.js';
+
+const getOrders = (req, res) => {
+  Order.find()
+    .populate('user products')
+    .exec((err, orders) => {
+      if (err) {
+        res.status(404).json({ message: err.message, success: false });
+      } else {
+        res.status(200).json({
+          message: 'Orders Found Successfully',
+          success: true,
+          data: orders,
+        });
+      }
+    });
+};
+
+const createOrder = (req, res) => {
+  Order.create(req.body, (err, order) => {
+    if (err) {
+      res.status(404).json({ message: err.message, success: false });
+    } else {
+      res.status(200).json({
+        message: 'Order Created Successfully',
+        success: true,
+        data: order,
+      });
+    }
+  });
+};
+
+export { getOrders, createOrder };
